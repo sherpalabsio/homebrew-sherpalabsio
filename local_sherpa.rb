024ffbca08f86d523ec5df336ba11f50346da2cf
Peter Toth <tothpeter08@gmail.com>
2025-03-15 22:09:59 +0700

    Local Sherpa v0.1.1

--- local_sherpa.rb
+++ local_sherpa.rb
@@ -4,9 +4,9 @@
 class LocalSherpa < Formula
   desc "Sherpa loads and unloads functions, aliases, and variables on a per-directory basis in your shell."
   homepage "https://github.com/SherpaLabsIO/local_sherpa"
-  url "https://github.com/sherpalabsio/local_sherpa/releases/download/v0.1.0/local_sherpa_0.1.0.tar.gz"
-  sha256 "c41615cf5efe0829d22faad257da2976598c1e646ff57a39e5d7354517b0ace7"
-  version "0.1.0"
+  url "https://github.com/sherpalabsio/local_sherpa/releases/download/v0.1.1/local_sherpa_0.1.1.tar.gz"
+  sha256 "66c21c7862de4329f711d2c7632ed52a7f41e662f267a81c8fb8819f0773bf86"
+  version "0.1.1"
 
   depends_on "bash"
 
@@ -15,16 +15,19 @@ class LocalSherpa < Formula
     bin.install_symlink libexec / "init" => "local_sherpa_init"
   end
 
-  def caveats
+  def post_install
     shell = ENV["SHELL"].split("/").last
 
     unless %w[zsh bash].include?(shell)
-      return <<~CAVEATS
+      puts <<~CAVEATS
         Local Sherpa only supports Zsh and Bash. It seems that you are using #{shell}.
       CAVEATS
+      return
     end
 
-    <<~CAVEATS
+    ohai "Post install notes"
+
+    puts <<~CAVEATS
       Don't forget to add the following line to your ~/.#{shell}rc file:
       eval \"$(local_sherpa_init)\"
       or run the following command:
